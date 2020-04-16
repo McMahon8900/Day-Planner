309babc75765f201c052cacebe88f6e9d51fabdc
Linsay McMahon <McMahon8900@gmail.com>
2020-04-15 22:45:52 -0500

    added color change function to highlight past, present and future timeblocks

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
 const m = moment();
 
-
-
-//Display the current day
+//Console log current day
 console.log(m.format("dddd, MMMM Do"));
-
+//Display the current day
 $("#currentDay").text(m.format("dddd, MMMM Do"));
+
+//Day Planner is aware of current time
+//Current hour is highlighted Red
+//Future hour is highlighted Green
+//Past hour is highlighted Grey
+
+function colorHrChange () {
+    var currentTime = moment().hours();
+    
+    $(".input").each(function () {
+        var plannerTime = parseInt($(this).attr("id"));
+        //parsing string "id" from document and displaying
+        //integer from index.html
+        console.log(plannerTime);
+        //if current time is greater than planner time 
+        //add past (Grey)
+        if (currentTime > plannerTime) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        //else if current time is less than planner time
+        //add future (Green)
+        } else if (currentTime < plannerTime) {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        } else {
+        //else add present (Red) for current hour
+            $(this).removeClass("future");
+            $(this).removeClass("past");
+            $(this).addClass("present");
+        }
+    });
+}
+
+$(document).ready( function() {
+    colorHrChange ();
+});
 
